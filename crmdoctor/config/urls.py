b75ed62e0da6e4b0965019e9f4743fde0e7fa58a
Alexander Chursin <37036721+AlexaDevel@users.noreply.github.com>
2021-12-23 12:14:36 +0300

    Update urls.py

--- crmdoctor/config/urls.py
+++ crmdoctor/config/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from ninja import NinjaAPI
 from app.views import tg_router, config_router, util_router
 
+admin.site.site_header = 'Doccrm Админ панель пациента'
+admin.site.index_title = 'Doccrm конфиг бота и данные о пациенте'
+admin.site.site_title = 'Doccrm Админ панель пациента'
+
 api = NinjaAPI(title='API MESSENGER', description='Сервис интеграции мессенжеров CRM-DOC с основным бекендом', docs_url='/swagger')
 api.add_router('/api', tg_router, tags=['Telegram'])
 api.add_router('/api', config_router, tags=['Text config'])
